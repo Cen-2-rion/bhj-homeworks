478d13fb9bee9f7fdb530f9fb484e59d643fddb1
Aleksandr Atrohov <atrohov_82@mail.ru>
2024-04-09 18:22:40 +0500

    Update mole-game

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -2,15 +2,22 @@ const getHole = index => document.getElementById(`hole${index}`);
 const dead = document.getElementById('dead');
 const lost = document.getElementById('lost');
 
+clear = () => {
+  dead.textContent = 0;
+  lost.textContent = 0;
+}
+
 for (let i = 1; i <= 10; i++) {
   getHole(i).onclick = () => {
     getHole(i).classList.contains('hole_has-mole') ? dead.textContent++ : lost.textContent++;
     
     if (dead.textContent == 10) {
       alert('Победа!');
+      clear();
     }
     if (lost.textContent == 5) {
       alert('Вы проиграли');
+      clear();
     }
   }
 }
