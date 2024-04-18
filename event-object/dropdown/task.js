9389627b64e01184d6e44627de4dfd7c805e0150
Aleksandr Atrohov <atrohov_82@mail.ru>
2024-04-19 02:43:06 +0500

    Update

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,17 +1,14 @@
-const item = document.querySelectorAll('.dropdown__item');
 const list = document.querySelector('.dropdown__list');
 let value = document.querySelector('.dropdown__value');
 
-value.addEventListener('click', () => {
+value.addEventListener('click', function() {
     list.classList.toggle('dropdown__list_active');
-    
+})
+
+list.addEventListener('click', (e) => {
     if (list.classList.contains('dropdown__list_active')) {
-        item.forEach(i => {
-            i.addEventListener('click', (event) => {
-                event.preventDefault();
-                value.innerHTML = i.textContent;
-                list.classList.remove('dropdown__list_active');
-            })
-        })
+        e.preventDefault();
+        value.innerHTML = e.target.textContent;
+        list.classList.remove('dropdown__list_active');
     }
 })
