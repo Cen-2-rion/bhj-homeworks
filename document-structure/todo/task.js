d920707d5a0a3d0121ad92e8512c8f3ef4b386d2
Aleksandr Atrohov <atrohov_82@mail.ru>
2024-05-10 19:58:44 +0500

    update

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -2,40 +2,41 @@
 const button = document.querySelector('.tasks__add');
 const input = document.querySelector('.tasks__input');
 const list = document.querySelector('.tasks__list');
+
 // вешаем обработчик события на кнопку
 button.addEventListener('click', (event) => {
     event.preventDefault();
     addTask();
 });
+
 // функция добавления новой задачи
 function addTask() {
     let text = input.value;
+
 // проверяем на пустую строку, если строка пустая, то ничего не делаем
-    if (text === '') {
+    if (text.trim() === '') {
         return;
     };
-// создаем элементы
-    let divTask = document.createElement('div');
-    divTask.classList.add('task');
-    let divTaskTitle = document.createElement('div');
-    divTaskTitle.classList.add('task__title');
-    divTaskTitle.innerText = text;
-    let a = document.createElement('a');
-    a.setAttribute('href', '#');
-    a.classList.add('task__remove');
-    a.innerHTML = '&times;';
-// присваиваем элементы в DOM дерево
-    divTask.appendChild(divTaskTitle);
-    divTask.appendChild(a);
-    list.appendChild(divTask);
+
+// добавляем новую задачу в список задач
+    list.insertAdjacentHTML("afterbegin", `
+    <div class="task">
+        <div class="task__title">
+            ${input.value}
+        </div>
+        <a href="#" class="task__remove">&times;</a>
+    </div>
+    `);
+
 // очищаем поле ввода и фокусируемся на нём
     input.value = '';
     input.focus();
-// вешаем обработчик события на кнопку удаления
-    a.addEventListener('click', removeTask);
+
+// находим кнопку удаления задачи и добавляем обработчик события
+    document.querySelector('.task__remove').addEventListener('click', removeTask);
 }
+
 // функция удаления задачи
 function removeTask(event) {
-    event.preventDefault();
-    event.target.parentNode.remove();
+    event.target.closest('.task').remove();
 }
