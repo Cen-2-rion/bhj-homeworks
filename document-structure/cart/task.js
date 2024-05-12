4fd6a749f944ed2fefd16874662dba9b4df88b42
Aleksandr Atrohov <atrohov_82@mail.ru>
2024-05-12 05:16:01 +0500

    update

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -5,11 +5,11 @@ const product = document.querySelectorAll('.product');
 product.forEach(elem => {
     elem.addEventListener('click', (event) => {
         let target = event.target;
-        let prodId = target.closest('.product').getAttribute('data-id'); // id продукта
-        let prodImg = target.closest('.product').querySelector('.product__image').src; // картинка продукта
-        let prodValue = elem.querySelector('.product__quantity-value').textContent; // количество товара продукта
-        let cart = document.querySelector('.cart__products'); // карточка товара в корзине
-        let cartId = cart.querySelector(`.cart__product[data-id="${prodId}"]`); // id карточки товара в корзине
+        const prodId = target.closest('.product').getAttribute('data-id'); // id продукта
+        const prodImg = target.closest('.product').querySelector('.product__image').src; // картинка продукта
+        const prodValue = elem.querySelector('.product__quantity-value').textContent; // количество товара продукта
+        const basket = document.querySelector('.cart__products'); // корзина
+        const cartId = basket.querySelector(`.cart__product[data-id="${prodId}"]`); // id карточки товара в корзине
 
 // уменшаем количество товара
         if (target.classList.contains('product__quantity-control_dec')) {
@@ -19,21 +19,19 @@ product.forEach(elem => {
             if (count.textContent < 1) {
                 count.textContent = 1;
             };
-            return;
         };
 
 // увеличиваем количество товара
         if (target.classList.contains('product__quantity-control_inc')) {
             let count = target.previousElementSibling;
             count.textContent++;
-            return;
         };
 
 // добавляем товар в корзину, если такого товара ещё нет, если есть, то увеличиваем количество товара в корзине
-        if (cartId) {
-            cartId.querySelector('.cart__product-count').textContent = Number(cartId.querySelector('.cart__product-count').textContent) + Number(prodValue); // увеличиваем количество товара в карточке
-        } else {
-            cart.insertAdjacentHTML('afterBegin', `
+        if (cartId && target.classList.contains('product__add')) {
+            cartId.querySelector('.cart__product-count').textContent = Number(cartId.querySelector('.cart__product-count').textContent) + Number(prodValue);
+        } else if (target.classList.contains('product__add')) {
+            basket.insertAdjacentHTML('afterBegin', `
             <div class="cart__product" data-id="${prodId}">
                 <img class="cart__product-image" src=${prodImg}>
                 <div class="cart__product-count">${prodValue}</div>
