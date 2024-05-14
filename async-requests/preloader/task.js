13c4270b0cc9107bb8ae06eef5ccc73953a3bb03
Aleksandr Atrohov <atrohov_82@mail.ru>
2024-05-15 04:40:26 +0500

    first commit preloader

---
+++ async-requests/preloader/task.js
@@ -0,0 +1,37 @@
+// создаём экземпляр объекта XMLHttpRequest
+const xhr = new XMLHttpRequest();
+
+// вешаем обработчик на запрос
+xhr.addEventListener('readystatechange', (event) => {
+    event.preventDefault();
+
+    const item = document.querySelector('.item');
+    const response = JSON.parse(xhr.responseText).response.Valute; // распарсиваем ответ
+
+    const loader = document.getElementById('loader');
+    loader.classList.remove('loader_active'); // отключаем загрузочный экран
+
+// по готовности проходимся циклом и находим необходимые значения
+    for (let elem in response) {
+    
+        if (xhr.readyState === xhr.DONE) {
+            item.insertAdjacentHTML('afterEnd', `
+            <div class="item">
+                <div class="item__code">
+                    ${response[elem].CharCode}
+                </div>
+                <div class="item__value">
+                    ${response[elem].Value}
+                </div>
+                <div class="item__currency">
+                    руб.
+                </div>
+            </div>
+            `);
+        }
+    }
+});
+
+// создаём асинхронный запрос и отправляем его
+xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/slow-get-courses');
+xhr.send();
