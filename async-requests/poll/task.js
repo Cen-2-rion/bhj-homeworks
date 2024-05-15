735d08620788709b3b1a522493ad2d47ef5158a9
Aleksandr Atrohov <atrohov_82@mail.ru>
2024-05-15 09:33:55 +0500

    first commit poll

---
+++ async-requests/poll/task.js
@@ -0,0 +1,36 @@
+// создаём экземпляр объекта XMLHttpRequest
+const xhr = new XMLHttpRequest();
+
+// вешаем обработчик на запрос
+xhr.addEventListener('readystatechange', (event) => {
+    event.preventDefault();
+
+    if (xhr.readyState === xhr.DONE) {
+
+        const response = JSON.parse(xhr.responseText);
+        const pollTitle = document.getElementById('poll__title');
+        const pollAnswers = document.getElementById('poll__answers');
+        
+        pollTitle.textContent = response.data.title;
+
+// перебираем вопросы в запросе и вставляем в DOM дерево
+        for (let answer of response.data.answers) {
+            pollAnswers.insertAdjacentHTML('afterBegin', `
+            <button class="poll__answer">
+                ${answer}
+            </button>
+            `);
+        }
+    }
+
+// перебираем кнопки и вешаем обработчик
+    document.querySelectorAll('.poll__answer').forEach(button => {
+        button.addEventListener('click', () => {
+            alert('«Спасибо, ваш голос засчитан!»');
+        });
+    });
+});
+
+// создаём асинхронный запрос и отправляем его
+xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/poll');
+xhr.send();
