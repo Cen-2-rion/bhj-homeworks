915a9ee57e6c0eacc4e0dab4d80fde01b0044999
Aleksandr Atrohov <atrohov_82@mail.ru>
2024-05-23 02:44:30 +0500

    first commit popup

---
+++ client-state/popup/task.js
@@ -0,0 +1,21 @@
+// находим необходимые элементы
+const modal = document.getElementById('subscribe-modal');
+const modalClose = document.querySelector('.modal__close');
+
+ // если модальное окно отсутствует, то показываем его
+if (!modal.classList.contains('modal_active')) {
+    modal.classList.add('modal_active');
+}
+
+// закрываем модальное окно по клику на крестик и устанавливаем в cookie информацию о закрытии
+modalClose.addEventListener('click', () => {
+    modal.classList.remove('modal_active');
+    document.cookie = 'modal_close=true; path=/';
+});
+
+// если в куке нет информации о закрытии страницы, то показываем окно, иначе скрываем окно
+if (document.cookie.indexOf('modal_close') === -1) {
+    modal.classList.add('modal_active');
+} else {
+    modal.classList.remove('modal_active');
+}
