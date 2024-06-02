055f68314c7bf43e7511fd9b5aaf18fc3bb275a2
Cen-2-rion <atrohov1982@gmail.com>
2024-06-02 15:27:38 +0300

    editing

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -13,12 +13,12 @@ button.addEventListener('click', (event) => {
 function addTask() {
     let text = input.value;
 
-// проверяем на пустую строку, если строка пустая, то ничего не делаем
+    // проверяем на пустую строку, если строка пустая, то ничего не делаем
     if (text.trim() === '') {
         return;
-    };
+    }
 
-// добавляем новую задачу в список задач
+    // добавляем новую задачу в список задач
     list.insertAdjacentHTML("afterbegin", `
     <div class="task">
         <div class="task__title">
@@ -28,15 +28,31 @@ function addTask() {
     </div>
     `);
 
-// очищаем поле ввода и фокусируемся на нём
+    // сохраняем задачу в локальное хранилище
+    localStorage.setItem('tasks', list.innerHTML);
+
+    // очищаем поле ввода и фокусируемся на нём
     input.value = '';
     input.focus();
 
-// находим кнопку удаления задачи и добавляем обработчик события
+    // находим кнопку удаления задачи и добавляем обработчик события
     document.querySelector('.task__remove').addEventListener('click', removeTask);
 }
 
 // функция удаления задачи
 function removeTask(event) {
-    event.target.closest('.task').remove();
+    event.target.closest('.task').remove(); // удаляем задачу из списка задач
+    localStorage.setItem('tasks', list.innerHTML); // сохраняем оставшиеся задачи в локальное хранилище
 }
+
+// сохраняем задачи после перезагрузки страницы
+window.addEventListener('load', () => {
+    
+    // если в локальном хранилище есть задачи, то добавляем их в список задач 
+    if (localStorage.getItem('tasks')) {
+        list.innerHTML = localStorage.getItem('tasks');
+    }
+    
+    // находим кнопку удаления задачи и добавляем обработчик события
+    document.querySelectorAll('.task__remove').forEach(item => item.addEventListener('click', removeTask));
+});
