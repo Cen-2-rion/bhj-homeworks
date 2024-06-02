69ceca78e613954238504437072f1b6450cde9b6
Cen-2-rion <atrohov1982@gmail.com>
2024-06-02 23:54:25 +0300

    editing

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,23 +1,16 @@
+// находим необходимые элементы
 const buttons = document.querySelectorAll('.font-size');
-const font = document.getElementById('book');
+const book = document.getElementById('book');
 
-buttons.forEach((button, i) => {
+// добавляем обработчик события на каждую кнопку
+buttons.forEach(button => {
     button.addEventListener('click', (e) => {
         e.preventDefault();
-        buttons.forEach(button => button.classList.remove('font-size_active'));
-        button.classList.add('font-size_active')
-        
-        switch (buttons[i].classList.contains('font-size_active')) {
-            case i === 0:
-                font.classList.add('book_fs-small');
-                font.classList.remove('book_fs-big');
-                break;
-            case i === 2:
-                font.classList.add('book_fs-big');
-                font.classList.remove('book_fs-small');
-                break;
-            default:
-                font.classList.remove('book_fs-small', 'book_fs-big');
-        }
-    })
-})
+        buttons.forEach(button => button.classList.remove('font-size_active')); // убираем активный класс у всех кнопок
+        button.classList.add('font-size_active'); // добавляем активный класс к нажатой кнопке
+
+        // удаляем классы у книги и добавляем класс с data-атрибутом size
+        book.classList.remove('book_fs-small', 'book_fs-big');
+        book.classList.add(`book_fs-${button.dataset.size}`);
+    });
+});
