7545030cadf1eaffb6dd1954e7abd83471b6e956
Cen-2-rion <atrohov1982@gmail.com>
2024-06-03 00:21:12 +0300

    editing

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -2,12 +2,14 @@ function displayElement() {
     const reveal = document.querySelectorAll('.reveal');
 
     reveal.forEach(elem => {
-        const {top, bottom} = elem.getBoundingClientRect();
+        const {top, bottom} = elem.getBoundingClientRect(); // находим координаты элемента
         
+        // если координаты элемента в пределах видимости окна, то отображаем элемент, иначе скрываем
         (top > 0 && bottom < window.innerHeight) ? elem.classList.add('reveal_active') : elem.classList.remove('reveal_active');
-    })
+    });
 }
 
+// запускаем функцию displayElement каждые 0 миллисекунд и вешаем обработчик скролла на окно
 setInterval(() => {
     window.addEventListener('scroll', displayElement(this.reveal));
 }, 0);
