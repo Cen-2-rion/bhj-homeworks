5c28d74e9d0aef916f456bd07513fe1f3272898a
Cen-2-rion <atrohov1982@gmail.com>
2024-06-02 15:26:28 +0300

    editing

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -11,23 +11,23 @@ product.forEach(elem => {
         const basket = document.querySelector('.cart__products'); // корзина
         const cartId = basket.querySelector(`.cart__product[data-id="${prodId}"]`); // id карточки товара в корзине
 
-// уменшаем количество товара
+        // уменшаем количество товара
         if (target.classList.contains('product__quantity-control_dec')) {
             let count = target.nextElementSibling;
             count.textContent--;
 
             if (count.textContent < 1) {
                 count.textContent = 1;
-            };
-        };
+            }
+        }
 
-// увеличиваем количество товара
+        // увеличиваем количество товара
         if (target.classList.contains('product__quantity-control_inc')) {
             let count = target.previousElementSibling;
             count.textContent++;
-        };
+        }
 
-// добавляем товар в корзину, если такого товара ещё нет, если есть, то увеличиваем количество товара в корзине
+        // добавляем товар в корзину, если такого товара ещё нет, если есть, то увеличиваем количество товара в корзине
         if (cartId && target.classList.contains('product__add')) {
             cartId.querySelector('.cart__product-count').textContent = Number(cartId.querySelector('.cart__product-count').textContent) + Number(prodValue);
         } else if (target.classList.contains('product__add')) {
@@ -37,6 +37,6 @@ product.forEach(elem => {
                 <div class="cart__product-count">${prodValue}</div>
             </div>
             `);
-        };
+        }
     });
 });
