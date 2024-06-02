ef0e155a95a2b0f09830e77e88117188751f1c52
Cen-2-rion <atrohov1982@gmail.com>
2024-06-03 00:35:10 +0300

    editing

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,14 +1,19 @@
+// находим необходимые элементы
 const list = document.querySelector('.dropdown__list');
 let value = document.querySelector('.dropdown__value');
 
+// добавляем обработчик события на клик по value
 value.addEventListener('click', function() {
-    list.classList.toggle('dropdown__list_active');
-})
+    list.classList.toggle('dropdown__list_active'); // добавляем или убираем класс для активного списка
+});
 
+// добавляем обработчик события на клик по элементам списка
 list.addEventListener('click', (e) => {
+    
+    // если список активный, то отменяем действие по умолчанию и убираем класс активного списка
     if (list.classList.contains('dropdown__list_active')) {
         e.preventDefault();
         value.innerHTML = e.target.textContent;
         list.classList.remove('dropdown__list_active');
     }
-})
+});
