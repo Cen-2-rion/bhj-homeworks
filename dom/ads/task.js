ea7d14558374532afe20b9dba0b8f5691bc22103
Cen-2-rion <atrohov1982@gmail.com>
2024-06-02 22:27:31 +0300

    editing

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,12 +1,21 @@
+// находим необходимые элементы
 const rotatorCases = document.querySelectorAll('.rotator__case ');
 let count = 0;
 
+// функция для смены слайдов
 function changeRotatorCases() {
 
+    // удаляем активный класс у всех слайдов и добавляем активный класс только к текущему слайду
     rotatorCases.forEach((elem, index) => {
         (count === index) ? elem.classList.add('rotator__case_active') : elem.classList.remove('rotator__case_active');
-    })
+        elem.style.color = elem.dataset.color; // меняем цвет текста через data-атрибут
+    });
+    
+    // увеличиваем счетчик на единицу и делаем проверку на количество слайдов
     count = (count + 1) % rotatorCases.length;
+
+    // запускаем функцию с data-атрибутом speed
+    setTimeout(changeRotatorCases, rotatorCases[count].dataset.speed);
 }
 
-setInterval(changeRotatorCases, 1000);
+changeRotatorCases();
