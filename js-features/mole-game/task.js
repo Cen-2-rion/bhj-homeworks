aa1de907bbc1e2ccfa71482a35a533007fd28e68
Cen-2-rion <atrohov1982@gmail.com>
2024-06-03 02:31:34 +0300

    editing

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,20 +1,25 @@
+// находим необходимые элементы
 const getHole = index => document.getElementById(`hole${index}`);
 const dead = document.getElementById('dead');
 const lost = document.getElementById('lost');
 
+// обнуляем счетчики
 clear = () => {
   dead.textContent = 0;
   lost.textContent = 0;
 }
 
+// добавляем слушателей на каждую дырку
 for (let i = 1; i <= 10; i++) {
   getHole(i).onclick = () => {
+    // если при клике в дырке был крот, то засчитываем победу, иначе поражение
     getHole(i).classList.contains('hole_has-mole') ? dead.textContent++ : lost.textContent++;
     
     if (dead.textContent == 10) {
       alert('Победа!');
       clear();
     }
+    
     if (lost.textContent == 5) {
       alert('Вы проиграли');
       clear();
