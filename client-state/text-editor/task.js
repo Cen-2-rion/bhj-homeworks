2853857b83a109725e700b88051c8e8f954afe2c
Cen-2-rion <atrohov1982@gmail.com>
2024-09-18 14:24:14 +0300

    update

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,15 +1,12 @@
 const editor = document.getElementById('editor'); // находим поле текста
-
-// добавляем кнопку "Очистить содержимое"
-const div = document.createElement('div');
-div.innerHTML = '<button id="clear">Очистить содержимое</button>';
-editor.insertAdjacentElement('afterEnd', div);
-
 const btnClear = document.getElementById('clear'); // находим кнопку "Очистить содержимое"
 
+// добавляем кнопку "Очистить содержимое" в конец поля текста
+editor.insertAdjacentElement('afterEnd', btnClear);
+
 // при изменении содержимого поля текста, сохраняем его в localStorage
-editor.addEventListener('input', (event) => {
-    localStorage.setItem('editor', JSON.stringify(event.target.value));
+editor.addEventListener('input', (e) => {
+    localStorage.setItem('editor', e.target.value);
 });
 
 // при нажатии на кнопку "Очистить содержимое", очищаем содержимое поля текста и удаляем сохранённое в localStorage
@@ -19,6 +16,4 @@ btnClear.addEventListener('click', () => {
 });
 
 // если в localStorage есть сохранённое содержимое поля текста, загружаем его в поле текста
-if (localStorage.getItem('editor')) {
-    editor.value = JSON.parse(localStorage.getItem('editor'));
-}
+editor.value = localStorage.getItem('editor');
