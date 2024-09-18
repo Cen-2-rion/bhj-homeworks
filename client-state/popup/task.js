fd8d42caf2f748ad6a40f75305fa77f049ed197c
Cen-2-rion <atrohov1982@gmail.com>
2024-09-18 23:57:55 +0300

    update task.js

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -2,16 +2,13 @@
 const modal = document.getElementById('subscribe-modal');
 const modalClose = document.querySelector('.modal__close');
 
-// если модальное окно отсутствует, то показываем его
-if (!modal.classList.contains('modal_active')) {
-    modal.classList.add('modal_active');
-}
-
 // закрываем модальное окно по клику на крестик и устанавливаем в cookie информацию о закрытии
 modalClose.addEventListener('click', () => {
     modal.classList.remove('modal_active');
     document.cookie = 'modal_close=true; path=/';
 });
 
-// если в куке нет информации о закрытии страницы, то показываем окно, иначе скрываем окно
-document.cookie.indexOf('modal_close') === -1 ? modal.classList.add('modal_active') : modal.classList.remove('modal_active');
+// если в куке нет информации о закрытии страницы, то показываем окно
+if (document.cookie.indexOf('modal_close') === -1 && document.cookieValue !== 'true') {
+    modal.classList.add('modal_active');
+}
